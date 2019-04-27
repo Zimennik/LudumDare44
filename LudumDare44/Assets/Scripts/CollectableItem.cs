c69ab9bc8b010c6dace4b072c34bf7eb46721730
Karasik <zimennik@gmail.com>
2019-04-27 05:42:11 +0300

    Empty project

---
+++ LudumDare44/Assets/Scripts/CollectableItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableItem : MonoBehaviour, IInteractable
+{
+  
+    public virtual void Interact()
+    {
+        
+    }
+}
