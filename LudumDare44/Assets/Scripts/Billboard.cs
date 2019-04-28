0522b68c7657f491ce3aa6c9fde07c6376f6b65e
Karasik <zimennik@gmail.com>
2019-04-28 23:12:50 +0300

    New billboard, new graphics setting

--- LudumDare44/Assets/Scripts/Billboard.cs
+++ LudumDare44/Assets/Scripts/Billboard.cs
@@ -6,13 +6,20 @@ public class Billboard : MonoBehaviour
 {
     private Transform _target;
 
+    private Renderer _renderer;
+
     void Awake()
     {
         _target = Camera.main.transform;
+        _renderer = GetComponent<Renderer>();
     }
-    
+
     void Update()
     {
+        if (_renderer != null)
+        {
+            _renderer.enabled = Vector3.Distance(_target.position, transform.position) < 10;
+        }
         transform.LookAt(_target);
     }
 }
