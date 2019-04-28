069fbbcd12c9ac5bed38b1196fd68a708430bc2e
Karasik <zimennik@gmail.com>
2019-04-28 23:54:17 +0300

    New test settings

---
+++ LudumDare44/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+   public static MusicManager Instance;
+
+   public AudioSource source;
+   
+   public void Awake()
+   {
+      Instance = this;
+   }
+
+   public void Play()
+   {
+      source.Play();
+   }
+
+   public void Stop()
+   {
+      source.Stop();
+   }
+
+
+}
